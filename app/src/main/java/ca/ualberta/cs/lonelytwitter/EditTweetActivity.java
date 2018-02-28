d1e5d32a9fa6748acb15888754c172d042304172
Carolyn Binns <cbinns@ualberta.ca>
2018-02-27 18:56:25 -0700

    lab 7

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/EditTweetActivity.java
@@ -0,0 +1,14 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class EditTweetActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_tweet);
+    }
+
+}
